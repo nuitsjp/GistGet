bfb260174839b3392488dc244bdb216b2a7b832f
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-23 08:48:05 +0900

    Add support for retrieving Gist packages by GistId; enhance tests for GistId handling

--- src/Private/Get-GistGetPackages.ps1
+++ src/Private/Get-GistGetPackages.ps1
@@ -11,10 +11,24 @@ function Get-GistGetPackages {
         Write-Verbose "Getting Gist from $Path"
         $yaml = Get-Content -Path $Path -Raw
     }
-    if($Uri)
-    {
+    elseif($Uri) {
         Write-Verbose "Getting Gist from $Uri"
         $yaml = Invoke-RestMethod -Uri $Uri
+    }
+    elseif($GistId)
+    {
+        # Get Gist information
+        Write-Verbose "Getting Gist for $GistId"
+        $gist = Get-GitHubGist -Gist $GistId
+
+        $fileName = $GistFileName
+        if (-not $fileName) {
+            # Get the first file if GistFileName is not specified
+            $fileName = $gist.files.PSObject.Properties.Name | Select-Object -First 1
+        }
+
+        # Get file contents
+        $yaml = $gist.files.$fileName.content
     }
 
     return ConvertTo-GistGetPackageFromYaml -Yaml $yaml
