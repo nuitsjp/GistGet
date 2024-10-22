d82ca438266ec03b0b89ec1ab7f9c293a2cb7f6b
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-23 08:40:34 +0900

    Add support for retrieving Gist packages from a URI; enhance tests for URI handling

--- src/Private/Get-GistGetPackages.ps1
+++ src/Private/Get-GistGetPackages.ps1
@@ -8,7 +8,13 @@ function Get-GistGetPackages {
     )
 
     if ($Path) {
+        Write-Verbose "Getting Gist from $Path"
         $yaml = Get-Content -Path $Path -Raw
+    }
+    if($Uri)
+    {
+        Write-Verbose "Getting Gist from $Uri"
+        $yaml = Invoke-RestMethod -Uri $Uri
     }
 
     return ConvertTo-GistGetPackageFromYaml -Yaml $yaml
--- test/Private/Get-GistGetPackages.Tests.ps1
+++ test/Private/Get-GistGetPackages.Tests.ps1
@@ -23,5 +23,26 @@ InModuleScope GistGet {
           $packages[2].PackageParameters | Should -Be ""
           $packages[2].Uninstall | Should -Be $true
       }
+
+      It "From Uri" {
+        # Arrange: テストの準備
+        
+        # Act: 関数を実行
+        $packages = Get-GistGetPackages -Uri "https://gist.githubusercontent.com/nuitsjp/82b27147c684502b4f9c4488f9fe6fa9/raw/a31f9016194b09b016def634765af9dedb13023c/Test-Load-Packages.yaml"
+        
+        # Assert: 結果が期待通りか確認
+        $packages.Count | Should -Be 3
+        $packages[0].Id | Should -Be "7zip.7zip"
+        $packages[0].PackageParameters | Should -Be ""
+        $packages[0].Uninstall | Should -Be $false
+
+        $packages[1].Id | Should -Be "Microsoft.VisualStudioCode.Insiders"
+        $packages[1].PackageParameters | Should -Be "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
+        $packages[1].Uninstall | Should -Be $false
+
+        $packages[2].Id | Should -Be "Zoom.Zoom"
+        $packages[2].PackageParameters | Should -Be ""
+        $packages[2].Uninstall | Should -Be $true
     }
+  }
 }
