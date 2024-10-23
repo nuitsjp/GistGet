b71ec2da506e97a50120605d0c6525ffa8bf4569
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-24 05:24:53 +0900

    Add cleanup step for GistId in Get-GistGetPackages.Tests; improve test isolation

--- test/Private/Get-GistGetPackages.Tests.ps1
+++ test/Private/Get-GistGetPackages.Tests.ps1
@@ -3,6 +3,12 @@ Import-Module -Name "$PSScriptRoot\..\..\src\GistGet.psd1" -Force
 
 InModuleScope GistGet {
     Describe "Get-GistGetPackages Tests" {
+        BeforeEach {
+            if (Get-ItemProperty -Path "HKCU:\Environment" -Name $Global:GistGetGistId -ErrorAction SilentlyContinue) {
+                Remove-ItemProperty -Path "HKCU:\Environment" -Name $Global:GistGetGistId
+            }
+        }
+
         It "From File" {
             # Arrange: テストの準備
             
@@ -25,24 +31,24 @@ InModuleScope GistGet {
         }
 
         It "From Uri" {
-        # Arrange: テストの準備
+            # Arrange: テストの準備
 
-        # Act: 関数を実行
-        $packages = Get-GistGetPackages -Uri "https://gist.githubusercontent.com/nuitsjp/82b27147c684502b4f9c4488f9fe6fa9/raw/a31f9016194b09b016def634765af9dedb13023c/Test-Load-Packages.yaml"
+            # Act: 関数を実行
+            $packages = Get-GistGetPackages -Uri "https://gist.githubusercontent.com/nuitsjp/82b27147c684502b4f9c4488f9fe6fa9/raw/a31f9016194b09b016def634765af9dedb13023c/Test-Load-Packages.yaml"
 
-        # Assert: 結果が期待通りか確認
-        $packages.Count | Should -Be 3
-        $packages[0].Id | Should -Be "7zip.7zip"
-        $packages[0].PackageParameters | Should -Be ""
-        $packages[0].Uninstall | Should -Be $false
+            # Assert: 結果が期待通りか確認
+            $packages.Count | Should -Be 3
+            $packages[0].Id | Should -Be "7zip.7zip"
+            $packages[0].PackageParameters | Should -Be ""
+            $packages[0].Uninstall | Should -Be $false
 
-        $packages[1].Id | Should -Be "Microsoft.VisualStudioCode.Insiders"
-        $packages[1].PackageParameters | Should -Be "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
-        $packages[1].Uninstall | Should -Be $false
+            $packages[1].Id | Should -Be "Microsoft.VisualStudioCode.Insiders"
+            $packages[1].PackageParameters | Should -Be "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
+            $packages[1].Uninstall | Should -Be $false
 
-        $packages[2].Id | Should -Be "Zoom.Zoom"
-        $packages[2].PackageParameters | Should -Be ""
-        $packages[2].Uninstall | Should -Be $true
+            $packages[2].Id | Should -Be "Zoom.Zoom"
+            $packages[2].PackageParameters | Should -Be ""
+            $packages[2].Uninstall | Should -Be $true
         }
 
         It "From GistId" {
