9ae029151a6cecf7b6e1fce369a6edf71afcc619
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-28 05:12:31 +0900

    Gist クラスの Provide メソッドに GistId とファイル名の検証を追加し、未指定の場合のエラーメッセージを実装。

--- src/Private/Gist.ps1
+++ src/Private/Gist.ps1
@@ -10,7 +10,12 @@ class Gist {
     static [Gist] Provide() {
         $id = [System.Environment]::GetEnvironmentVariable('GIST_GET_GIST_ID', [System.EnvironmentVariableTarget]::User)
         $fileName = [System.Environment]::GetEnvironmentVariable('GIST_GET_GIST_FILE_NAME', [System.EnvironmentVariableTarget]::User)
-
+        if($id -and $fileName) {
+            return [Gist]::new($id, $fileName)
+        }
+        else {
+            throw "Please specify a GistId or Path. Alternatively, you need to register the GistId in advance using Set-GistGetGistId."
+        }
 
     }
 }
