8879119e1014e383d34508008174af34bddadc2a
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-28 15:03:14 +0900

    Get-GistGetPackage 関数の GistId と GistFileName パラメータを Gist パラメータに統合し、関連するテストを更新

--- src/Public/Get-GistGetPackage.ps1
+++ src/Public/Get-GistGetPackage.ps1
@@ -1,80 +1,10 @@
-<#
-.SYNOPSIS
-    Retrieves GistGet package information from various sources.
-
-.DESCRIPTION
-    This function retrieves package information for GistGet from one of three possible sources:
-    - A GitHub Gist (using GistId)
-    - A remote URL
-    - A local file path
-    
-    If no source is specified, it attempts to retrieve the GistId from environment variables.
-    The retrieved YAML content is then converted to GistGetPackage objects.
-
-.PARAMETER GistId
-    The ID of the Gist containing the package information.
-
-.PARAMETER GistFileName
-    The specific filename within the Gist to retrieve. If not specified and using GistId, 
-    the first file in the Gist will be used.
-
-.PARAMETER Uri
-    The URL from which to retrieve the package information.
-
-.PARAMETER Path
-    The local file path from which to retrieve the package information.
-
-.EXAMPLE
-    PS> Get-GistGetPackage
-    Retrieves package information using the GistId stored in environment variables.
-
-.EXAMPLE
-    PS> Get-GistGetPackage -GistId "1234567890abcdef" -GistFileName "packages.yaml"
-    Retrieves package information from a specific file in the specified Gist.
-
-.EXAMPLE
-    PS> Get-GistGetPackage -Uri "https://example.com/packages.yaml"
-    Retrieves package information from a remote URL.
-
-.EXAMPLE
-    PS> Get-GistGetPackage -Path "C:\packages.yaml"
-    Retrieves package information from a local file.
-
-.NOTES
-    Priority order for source selection:
-    1. Path (if specified)
-    2. Uri (if specified)
-    3. GistId (if specified)
-    4. Environment variable GistId (if no other source specified)
-
-    Requirements:
-    - For Gist retrieval: GitHub authentication must be configured
-    - For URL retrieval: Internet connectivity required
-    - YAML content must be in the correct format for GistGetPackage conversion
-
-.OUTPUTS
-    [GistGetPackage[]]
-    Returns an array of GistGetPackage objects parsed from the YAML content.
-
-.LINK
-    Get-GistGetGistId
-    Get-GitHubGist
-#>
 function Get-GistGetPackage {
     [CmdletBinding()]
     param(
-        [string] $GistId,
-        [string] $GistFileName,
+        [Gist] $Gist,
         [string] $Uri,
         [string] $Path
     )
-
-    # If no arguments are specified, get GistId from environment variable
-    if (-not $GistId -and -not $Uri -and -not $Path) {
-        Write-Verbose "Getting GistId from environment variable"
-        $GistId = Get-GistGetGistId
-        Write-Verbose "Environment variable GistId: $GistId"
-    }
 
     if ($Path) {
         Write-Verbose "Getting Gist from $Path"
@@ -84,17 +14,13 @@ function Get-GistGetPackage {
         Write-Verbose "Getting Gist from $Uri"
         $yaml = Invoke-RestMethod -Uri $Uri
     }
-    elseif($GistId)
+    elseif($Gist)
     {
-        Write-Verbose "Getting Gist for $GistId"
-        $gist = Get-GitHubGist -Gist $GistId
-
-        $fileName = $GistFileName
-        if (-not $fileName) {
-            $fileName = $gist.files.PSObject.Properties.Name | Select-Object -First 1
-        }
-
-        $yaml = $gist.files.$fileName.content
+        $GistId = $Gist.Id
+        $GistFileName = $Gist.FileName
+        Write-Verbose "Getting Gist for id:$GistId fileName:$GistFileName"
+        $remoteGist = Get-GitHubGist -Gist $GistId
+        $yaml = $remoteGist.files.$GistFileName.content
     }
     else {
         throw "Please specify a GistId, Uri or Path. Alternatively, you need to register the GistId in advance using Set-GistGetGistId."
--- test/Public/Get-GistGetPackage.Tests.ps1
+++ test/Public/Get-GistGetPackage.Tests.ps1
@@ -58,54 +58,12 @@ InModuleScope GistGet {
             $packages[2].Uninstall | Should -Be $true
         }
 
-        It "From GistId" {
+        It "From Gist" {
             # Arrange: テストの準備
     
             # Act: 関数を実行
-            $packages = Get-GistGetPackage -GistId "82b27147c684502b4f9c4488f9fe6fa9"
-    
-            # Assert: 結果が期待通りか確認
-            $packages.Count | Should -Be 3
-            $packages[0].Id | Should -Be "7zip.7zip"
-            $packages[0].Override | Should -BeNullOrEmpty
-            $packages[0].Uninstall | Should -Be $false
-    
-            $packages[1].Id | Should -Be "Microsoft.VisualStudioCode.Insiders"
-            $packages[1].Override | Should -Be "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
-            $packages[1].Uninstall | Should -Be $false
-    
-            $packages[2].Id | Should -Be "Zoom.Zoom"
-            $packages[2].Override | Should -BeNullOrEmpty
-            $packages[2].Uninstall | Should -Be $true
-        }
-
-        It "From GistId and FileName" {
-            # Arrange: テストの準備
-    
-            # Act: 関数を実行
-            $packages = Get-GistGetPackage -GistId "82b27147c684502b4f9c4488f9fe6fa9" -GistFileName "GistGet.yaml"
-    
-            # Assert: 結果が期待通りか確認
-            $packages.Count | Should -Be 3
-            $packages[0].Id | Should -Be "7zip.7zip"
-            $packages[0].Override | Should -BeNullOrEmpty
-            $packages[0].Uninstall | Should -Be $false
-    
-            $packages[1].Id | Should -Be "Microsoft.VisualStudioCode.Insiders"
-            $packages[1].Override | Should -Be "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
-            $packages[1].Uninstall | Should -Be $false
-    
-            $packages[2].Id | Should -Be "Zoom.Zoom"
-            $packages[2].Override | Should -BeNullOrEmpty
-            $packages[2].Uninstall | Should -Be $true
-        }
-
-        It "From GistId environment variable" {
-            # Arrange: テストの準備
-            Set-ItemProperty -Path "HKCU:\Environment" -Name $Global:GistGetGistId -Value "82b27147c684502b4f9c4488f9fe6fa9"
-    
-            # Act: 関数を実行
-            $packages = Get-GistGetPackage
+            $gist = [Gist]::new("82b27147c684502b4f9c4488f9fe6fa9", "GistGet.yaml")
+            $packages = Get-GistGetPackage -Gist $gist
     
             # Assert: 結果が期待通りか確認
             $packages.Count | Should -Be 3
