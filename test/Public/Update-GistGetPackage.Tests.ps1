75e2588889eb97a00f10428d3d360bb31ac9f603
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-31 06:16:51 +0900

    Update Update-GistGetPackage.Tests.ps1 to add tests for package update scenarios and mock dependencies

--- test/Public/Update-GistGetPackage.Tests.ps1
+++ test/Public/Update-GistGetPackage.Tests.ps1
@@ -79,4 +79,68 @@ InModuleScope GistGet {
             Should -Not -Invoke Restart-Computer
         }
     }
+
+    Describe "Update-GistGetPackage Update, Exists, Exists, Equal" {
+        BeforeAll {
+            # モックの準備
+            Mock Get-GistGetPackage {
+                $package = [GistGetPackage]::new('NuitsJp.ClaudeToZenn')
+                $package.Version = '1.0.0'
+                return @(
+                    $package
+                )
+            }
+
+            Mock Get-WinGetPackage { 
+                return @(
+                    [PSCustomObject]@{ 
+                        Id = 'NuitsJp.ClaudeToZenn'
+                        IsUpdateAvailable = $true
+                        InstalledVersion = '1.0.0'
+                    },
+                    [PSCustomObject]@{ 
+                        Id = 'Foo'
+                        IsUpdateAvailable = $false
+                        InstalledVersion = '0.9.0'
+                    }
+                )
+            }
+
+            Mock Confirm-ReplacePackage { 
+                return $true
+            }
+    
+            Mock Uninstall-WinGetPackage { 
+                return [PSCustomObject]@{ RebootRequired = $false }
+            }
+            Mock Install-WinGetPackage { 
+            }
+            Mock Update-WinGetPackage { 
+            }
+            Mock Confirm-Reboot{
+            }
+            Mock Restart-Computer {
+            }
+        }
+
+        It "アップデート 
+        GistGetPackage:あり 
+        GistGetPackage.Version:あり 
+        GistGetPackage.Version=WinGetPackage.InstalledVersion" {
+            # Arrange: テストの準備
+
+            # Act: 関数を実行
+            Update-GistGetPackage
+
+            # Assert: 結果が期待通りか確認
+            Should -Invoke Get-GistGetPackage
+            Should -Invoke Get-WinGetPackage
+            Should -Not -Invoke Confirm-ReplacePackage
+            Should -Not -Invoke Uninstall-WinGetPackage
+            Should -Not -Invoke Install-WinGetPackage
+            Should -Not -Invoke Update-WinGetPackage
+            Should -Not -Invoke Confirm-Reboot
+            Should -Not -Invoke Restart-Computer
+        }
+    }
 }
