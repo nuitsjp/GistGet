dfaf5af716774e7ad14508b9899d0ef82993bcaf
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-10-31 06:18:45 +0900

    Update Update-GistGetPackage.Tests.ps1 to add tests for update scenarios with mocked dependencies

--- test/Public/Update-GistGetPackage.Tests.ps1
+++ test/Public/Update-GistGetPackage.Tests.ps1
@@ -80,6 +80,69 @@ InModuleScope GistGet {
         }
     }
 
+    Describe "Update-GistGetPackage Update, Exists, Exists, NotEqual, False" {
+        BeforeAll {
+            # モックの準備
+            Mock Get-GistGetPackage {
+                $package = [GistGetPackage]::new('NuitsJp.ClaudeToZenn')
+                $package.Version = '1.0.0'
+                return @(
+                    $package
+                )
+            }
+
+            Mock Get-WinGetPackage { 
+                return @(
+                    [PSCustomObject]@{ 
+                        Id = 'NuitsJp.ClaudeToZenn'
+                        IsUpdateAvailable = $true
+                        InstalledVersion = '0.9.0'
+                    },
+                    [PSCustomObject]@{ 
+                        Id = 'Foo'
+                        IsUpdateAvailable = $false
+                        InstalledVersion = '0.9.0'
+                    }
+                )
+            }
+
+            Mock Confirm-ReplacePackage { 
+                return $false
+            }
+    
+            Mock Uninstall-WinGetPackage {}
+            Mock Install-WinGetPackage {}
+            Mock Update-WinGetPackage {}
+            Mock Confirm-Reboot {}
+            Mock Restart-Computer {}
+        }
+
+        It "アップデート 
+        GistGetPackage:あり 
+        GistGetPackage.Version:あり 
+        GistGetPackage.Version≠WinGetPackage.InstalledVersion 
+        Confirm-ReplacePackage:False" {
+            # Arrange: テストの準備
+
+            # Act: 関数を実行
+            Update-GistGetPackage
+
+            # Assert: 結果が期待通りか確認
+            Should -Invoke Get-GistGetPackage
+            Should -Invoke Get-WinGetPackage
+            Should -Invoke Confirm-ReplacePackage -ParameterFilter {
+                $Id -eq 'NuitsJp.ClaudeToZenn' -and
+                $InstalledVersion -eq '0.9.0' -and
+                $GistGetVersion -eq '1.0.0'
+            }
+            Should -Not -Invoke Uninstall-WinGetPackage
+            Should -Not -Invoke Install-WinGetPackage
+            Should -Not -Invoke Update-WinGetPackage
+            Should -Not -Invoke Confirm-Reboot
+            Should -Not -Invoke Restart-Computer
+        }
+    }
+
     Describe "Update-GistGetPackage Update, Exists, Exists, Equal" {
         BeforeAll {
             # モックの準備
