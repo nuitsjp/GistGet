de1f3395ecdfcbc7b46d0d948415f965b22be93f
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-12-13 21:40:57 +0900

    DefaultGistFileName を GistGet.yaml に変更
    DefaultGistFileName 定数の値を "packages.yaml" から "GistGet.yaml" に変更しました。他の定数には影響ありません。

--- src/GistGet/GistGet/Constants.cs
+++ src/GistGet/GistGet/Constants.cs
@@ -3,7 +3,7 @@ namespace GistGet;
 public static class Constants
 {
     public const string ProductHeader = "GistGet";
-    public const string DefaultGistFileName = "packages.yaml";
+    public const string DefaultGistFileName = "GistGet.yaml";
     public const string DefaultGistDescription = "GistGet Packages";
     public const string ClientId = "Ov23lihQJhLB6hCnEIvS";
 }
